be581f91a36d7306bc18d1c90118e1fe974a300d
maxwen <max.weninger@gmail.com>
2015-12-04 15:46:36 +0100

    bring back screen record shortcut
    Change-Id: Iad561a2dc7f6c68b49e40537c4d5152d93dcdf09
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5687,6 +5687,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             mVolumeDownKeyTime = event.getDownTime();
                             mScreenshotChordVolumeDownKeyConsumed = false;
                             cancelPendingPowerKeyAction();
+                            cancelPendingScreenrecordChordAction();
                             interceptScreenshotChord();
                         }
                     } else {
@@ -5703,6 +5704,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             mScreenrecordChordVolumeUpKeyConsumed = false;
                             cancelPendingPowerKeyAction();
                             cancelPendingScreenshotChordAction();
+                            interceptScreenrecordChord();
                         }
                     } else {
                         mVolumeUpKeyTriggered = false;
