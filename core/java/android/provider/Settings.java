604af52b55555ddb6028736c60bc816ae4cbef1d
Austin T. Conn <atc3030dev@gmail.com>
2015-12-05 22:44:43 +0000

    Volume rocker music controls [1/2]
    Change-Id: I57a5b8ff05941863492acbd65a10ad0708c05c85
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3845,6 +3845,12 @@ public final class Settings {
         public static final String REMOVE_TILE_SUMMARY = "remove_tile_summary";
 
         /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
