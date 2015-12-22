7bc1cf5b198e7ee485b9dfdd6a98e9712087578e
Alex Cruz <mazdarider23@gmail.com>
2015-12-22 13:31:32 -0800

    QS: NavBar tile
    Benzo Rom Edits:
    Made to work with N
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -53,6 +53,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -447,6 +448,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("ime")) return new ImeTile(this);
+        else if (tileSpec.equals("navbar")) return new NavBarTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
