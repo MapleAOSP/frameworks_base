69214a0f27ad3ce26fb6c84262df42ce50fc44c6
LorDClockaN <davor@losinj.com>
2016-03-02 18:53:23 -0500

    Powermenu Torch option [1/2]
    - Ported up by @Mazda-- from Lollipop
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/fd7707194c9fb760233759c69ec7966b62cdee9c
    
    - Use CameraManager instead of TorchManager since we no longer use CM's torch fixes/implementation
    
    Credit to @beanstown106 for his commit here
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/49e3cd541f68b00c04b6dda03341c09ba9ba57f9
    
    Change-Id: Ia837d353082ed96199c784a764f8d9d772ec8281
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/com/android/internal/util/cm/PowerMenuConstants.java
+++ core/java/com/android/internal/util/cm/PowerMenuConstants.java
@@ -27,6 +27,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_SETTINGS = "settings";
     public static final String GLOBAL_ACTION_KEY_LOCKDOWN = "lockdown";
     public static final String GLOBAL_ACTION_KEY_BUGREPORT = "bugreport";
+    public static final String GLOBAL_ACTION_KEY_TORCH = "torch";
     public static final String GLOBAL_ACTION_KEY_SILENT = "silent";
 
     private static String[] ALL_ACTIONS = {
@@ -39,6 +40,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_SETTINGS,
         GLOBAL_ACTION_KEY_LOCKDOWN,
         GLOBAL_ACTION_KEY_BUGREPORT,
+        GLOBAL_ACTION_KEY_TORCH,
         GLOBAL_ACTION_KEY_SILENT
     };
 
