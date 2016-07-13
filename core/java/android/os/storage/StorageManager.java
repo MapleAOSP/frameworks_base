92305ea2dd08ee468c62029d7cc675ef040d3013
Tao Wu <lepton@google.com>
2016-07-13 16:29:02 -0700

    Handle exception when mount service not ready.
    I am trying to follow the same way we handel RemoteException
    in these two static functions.
    
    Change-Id: I4b6cc961ba585712ca600090a1956b139da7159d
    Signed-off-by: Tao Wu <lepton@google.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/os/storage/StorageManager.java
+++ core/java/android/os/storage/StorageManager.java
@@ -912,6 +912,9 @@ public class StorageManager {
     public static @NonNull StorageVolume[] getVolumeList(int userId, int flags) {
         final IMountService mountService = IMountService.Stub.asInterface(
                 ServiceManager.getService("mount"));
+        if (mountService == null) {
+            throw new RuntimeException("mount service not ready yet");
+        }
         try {
             String packageName = ActivityThread.currentOpPackageName();
             if (packageName == null) {
@@ -1215,6 +1218,9 @@ public class StorageManager {
     public static File maybeTranslateEmulatedPathToInternal(File path) {
         final IMountService mountService = IMountService.Stub.asInterface(
                 ServiceManager.getService("mount"));
+        if (mountService == null) {
+            return path;
+        }
         try {
             final VolumeInfo[] vols = mountService.getVolumes(0);
             for (VolumeInfo vol : vols) {
