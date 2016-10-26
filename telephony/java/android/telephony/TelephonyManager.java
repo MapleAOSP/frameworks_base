fde77348263ab181d8391c32bc74abd8701286ca
Joe Maples <joe@frap129.org>
2016-10-26 17:56:17 +0000

    Revert "SEEMP: add framework instrumentation"
    This reverts commit b3f447ee3025b405e9af6302ff3daf18c20adba3.

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -866,7 +866,6 @@ public class TelephonyManager {
      * @param slotId of which deviceID is returned
      */
     public String getDeviceId(int slotId) {
-        android.util.SeempLog.record_str(8, ""+slotId);
         // FIXME this assumes phoneId == slotId
         try {
             IPhoneSubInfo info = getSubscriberInfo();
@@ -962,7 +961,6 @@ public class TelephonyManager {
      * {@link android.Manifest.permission#ACCESS_COARSE_LOCATION ACCESS_FINE_LOCATION}.
      */
     public CellLocation getCellLocation() {
-        android.util.SeempLog.record(49);
         try {
             ITelephony telephony = getITelephony();
             if (telephony == null) {
@@ -1060,7 +1058,6 @@ public class TelephonyManager {
      */
     @Deprecated
     public List<NeighboringCellInfo> getNeighboringCellInfo() {
-        android.util.SeempLog.record(50);
         try {
             ITelephony telephony = getITelephony();
             if (telephony == null)
@@ -2069,7 +2066,6 @@ public class TelephonyManager {
      * @hide
      */
     public String getSimSerialNumber(int subId) {
-        android.util.SeempLog.record_str(388, ""+subId);
         try {
             IPhoneSubInfo info = getSubscriberInfo();
             if (info == null)
@@ -2159,7 +2155,6 @@ public class TelephonyManager {
      * @hide
      */
     public String getSubscriberId(int subId) {
-        android.util.SeempLog.record_str(389, ""+subId);
         try {
             IPhoneSubInfo info = getSubscriberInfo();
             if (info == null)
@@ -2248,7 +2243,6 @@ public class TelephonyManager {
      * @hide
      */
     public String getLine1Number(int subId) {
-        android.util.SeempLog.record_str(9, ""+subId);
         String number = null;
         try {
             ITelephony telephony = getITelephony();
